8af67e9d91b095b4944a6b13d2e9991d6a245afb
devFron <developermater26@gmail.com>
2021-11-25 17:04:47 -0600

    se modifico el como se inserta y se remueve el loader

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import Loader from './components/Loader.js'
 import Router from './router.js'
 const App = async ()=>{
     const $ROOT = document.getElementById('root') 
-    const $Loader = Loader()
-    $ROOT.appendChild($Loader)
+    const $Loader = document.createElement('div')
+    $Loader.appendChild(Loader())
+    $ROOT.insertAdjacentElement('beforebegin',$Loader)
     await Router()
-    $ROOT.removeChild($Loader)
+    $Loader.style.display = 'none'
 }
 export default App
