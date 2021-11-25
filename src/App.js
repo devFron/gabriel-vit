033ba060198d3362db7de40dbef2f1afbd738c5f
devFron <developermater26@gmail.com>
2021-11-25 14:42:35 -0600

    la funcion App se volvio asyncrona, se importaron los modulos de loader y router se ejecuta asyncronamente la funcion Router y al terminar el proceso se remueve el loader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-const App = ()=>{
-    console.log('hello Jose')
+import Loader from './components/Loader.js'
+import Router from './router.js'
+const App = async ()=>{
+    const $ROOT = document.getElementById('root') 
+    const $Loader = Loader()
+    $ROOT.appendChild($Loader)
+    await Router()
+    $ROOT.removeChild($Loader)
 }
 export default App
