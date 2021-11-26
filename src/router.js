0a814146513fdda1cd13048c786e973b07950a8c
devFron <developermater26@gmail.com>
2021-11-25 20:31:25 -0600

    se cambio el titulo de la pagina segun el hash validado

--- src/router.js
+++ src/router.js
@@ -2,15 +2,18 @@ import Home from './pages/home/home.js'
 const Router = async ()=>{
     const {hash} = location
     const $Root = document.getElementById('root')
+    const title = document.querySelector('title')
     $Root.innerHTML=''    
     if(!hash.includes('#/skill')){ 
         switch (hash) {
             case '':
                $Root.appendChild(Home())
+               title.textContent = 'Inicio'
             break;
 
             case '#/home':
-               $Root.appendChild(Home())
+                $Root.appendChild(Home())
+                title.textContent = 'Inicio'
             break;
        
            default:
