d578b7426bf819868809d7ccbc69ed67d5912e2d
devFron <developermater26@gmail.com>
2021-11-25 19:11:38 -0600

    se importo el componente home y se hacen validaciondes del hash para saver que UI retornar

--- src/router.js
+++ src/router.js
@@ -1,2 +1,13 @@
-const Router = async ()=>{}
+import Home from './pages/home/home.js'
+const Router = async ()=>{
+    const {hash} = location
+    const $Root = document.getElementById('root')
+    $Root.innerHTML=''    
+    if(!hash.includes('#/skill')){ 
+    }else{
+
+        console.log('skills')
+    }
+
+}
 export default Router
