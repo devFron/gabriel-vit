dbc5f8f57f3c8dcf9867909085744cd4a34d4638
devFron <developermater26@gmail.com>
2021-11-25 20:49:37 -0600

    se agrego un fondo para el body

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,7 +3,15 @@ const Home = ()=>{
     const $Home = document.createElement('section')
     const $style = document.getElementById('dynamic-styles')
     const style = `
+        body{
+            background:url(../src/assets/background-mobile.png);
+        }
         ${Header().componnetStyles}
+        @media screen and (min-width:700px){
+            body{
+                background:url(../src/assets/background-desktop.png);
+            }
+        }
     `
     $style.innerHTML = ''
     $style.innerHTML = style
