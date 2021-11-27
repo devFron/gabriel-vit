956c4ffbe54ddb3a45fb6cb109669f6d6a4617ca
devFron <developermater26@gmail.com>
2021-11-27 15:27:02 -0600

    se valida en cada iteracion si el la key se encunetra en el objeto si se retorna true la variable correspodiente a el breakpoin sera igual a el valor de la key del objeto y no sera igual a null

--- src/helpers/vit.js
+++ src/helpers/vit.js
@@ -7,8 +7,33 @@ const Styles = (stylesSet)=>{
 
     for (let i = 0; i < stylesSet.length; i++) {
         const el = stylesSet[i];
-        const {mobile,tablet,laptop,desktkop,extraLarge} = el
-    }
-        
+        const {mobile,tablet,laptop,desktop,extraLarge} = el
+
+        if('mobile' in el){
+            Mobile += mobile
+        }else{
+            Mobile += 'null'
+        }
+        if('tablet' in el){
+            Tablet += tablet
+        }else{
+            Tablet += 'null'
+        }
+        if('laptop' in el){
+            Laptop += laptop
+        }else{
+            Laptop += 'null'
+        }
+        if('desktop' in el){
+            Desktop += desktop
+        }else{
+            Desktop += 'null'
+        }
+        if('extraLarge' in el){
+            ExtraLarge += extraLarge
+        }else{
+            ExtraLarge += 'null'
+        }
+    }     
 }
 export default Styles
