fd1bb97d775bc06577c4e32c2a1b5de441501ff9
devFron <developermater26@gmail.com>
2021-11-27 16:10:57 -0600

    se importo la funcion styles de vit.js se recupero la etiqueta style del html se destructuro la funcion Home el nodo del dom se agrego como hijo de  y los estilos se le paso como parametro a la funcion Styles y la cadena que retorna sera el valor de la etiqueta style

--- src/router.js
+++ src/router.js
@@ -1,13 +1,18 @@
 import Home from './pages/home/home.js'
+import Styles from './helpers/vit.js'
 const Router = async ()=>{
     const {hash} = location
     const $Root = document.getElementById('root')
     const title = document.querySelector('title')
-    $Root.innerHTML=''    
+    const $style = document.getElementById('dynamic-styles')
+    $Root.innerHTML=''
+    $style.innerHTML = ''
     if(!hash.includes('#/skill')){ 
         switch (hash) {
             case '':
-               $Root.appendChild(Home())
+                const [componentHome,stylesHome] = Home()
+               $Root.appendChild(componentHome)
+               $style.innerHTML = Styles(stylesHome)
                title.textContent = 'Inicio'
             break;
 
