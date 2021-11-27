82898036a0b79b0fdc98cc5f588f682963a7ae30
devFron <developermater26@gmail.com>
2021-11-27 14:39:50 -0600

    se crearon lar variables que retornaran los estilos segun el breakpoint

--- src/helpers/vit.js
+++ src/helpers/vit.js
@@ -1,4 +1,8 @@
-const Styles = ()=>{
-    
+const Styles = (stylesSet)=>{
+    let Mobile = null
+    let Tablet = null
+    let Laptop = null
+    let Desktop = null
+    let ExtraLarge = null
 }
 export default Styles
