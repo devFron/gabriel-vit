5d1862399e882a5effa2ad5525a9b2264829c083
devFron <developermater26@gmail.com>
2021-11-27 16:34:33 -0600

    se los estilos de el principio seran los de el loader para agragar estilos de las paginas sera mas e igual a los del loader

--- src/router.js
+++ src/router.js
@@ -1,18 +1,19 @@
 import Home from './pages/home/home.js'
 import Styles from './helpers/vit.js'
+import Loader from './components/Loader.js'
 const Router = async ()=>{
     const {hash} = location
     const $Root = document.getElementById('root')
     const title = document.querySelector('title')
     const $style = document.getElementById('dynamic-styles')
     $Root.innerHTML=''
-    $style.innerHTML = ''
+    $style.innerHTML = Loader()[1].mobile
     if(!hash.includes('#/skill')){ 
         switch (hash) {
             case '':
                 const [componentHome,stylesHome] = Home()
                $Root.appendChild(componentHome)
-               $style.innerHTML = Styles(stylesHome)
+               $style.innerHTML += Styles(stylesHome)
                title.textContent = 'Inicio'
             break;
 
