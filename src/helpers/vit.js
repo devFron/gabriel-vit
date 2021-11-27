1c118bb934b6806f6ad40d181c57aed95939381e
devFron <developermater26@gmail.com>
2021-11-27 15:16:02 -0600

    la funcion Styles recibe un parametro el cual sera un arreglo de objetos cada uno de los objetos seran los estilos de un componente se itera ese arreglo y se destructuran cada uno se los arreglos

--- src/helpers/vit.js
+++ src/helpers/vit.js
@@ -4,5 +4,11 @@ const Styles = (stylesSet)=>{
     let Laptop = null
     let Desktop = null
     let ExtraLarge = null
+
+    for (let i = 0; i < stylesSet.length; i++) {
+        const el = stylesSet[i];
+        const {mobile,tablet,laptop,desktkop,extraLarge} = el
+    }
+        
 }
 export default Styles
