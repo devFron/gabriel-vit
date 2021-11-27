716dd773752323f9711076ea755340aa1eecf19a
devFron <developermater26@gmail.com>
2021-11-27 16:06:13 -0600

    se destructuro el componente header y el nodo se agrego como hijo de , el componente Home retornara un arreglo que contiene un nodo y un arreglo que contiene los estilos del componente Header

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,22 +1,15 @@
 import Header from '../../components/Header.js'
 const Home = ()=>{
     const $Home = document.createElement('section')
-    const $style = document.getElementById('dynamic-styles')
-    const style = `
-        body{
-            background:url(../src/assets/background-mobile.png);
-        }
-        ${Header().componnetStyles}
-        @media screen and (min-width:700px){
-            body{
-                background:url(../src/assets/background-desktop.png);
-            }
-        }
-    `
-    $style.innerHTML = ''
-    $style.innerHTML = style
-    $Home.appendChild(Header().componnet)
+    const [componentHeader,stylesHeader] = Header()
 
-    return $Home
+    $Home.appendChild(componentHeader)
+
+    return [
+        $Home,
+        [
+            stylesHeader
+        ]
+    ]
 }
 export default Home
