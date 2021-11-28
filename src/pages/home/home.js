5ef2200b8772a0e57a8711f1301cd99471e84861
devFron <developermater26@gmail.com>
2021-11-27 19:15:37 -0600

    se agregoimporto el componente de PresentacionCard y se destruturo para poder seprar el layaout y los estilos

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,14 +1,18 @@
 import Header from '../../components/Header.js'
+import PresentationCard from '../../components/Presentation_card.js'
 const Home = ()=>{
     const $Home = document.createElement('section')
     const [componentHeader,stylesHeader] = Header()
+    const [componentPresentationCard,stylesPresentationCard] = PresentationCard()
 
     $Home.appendChild(componentHeader)
+    $Home.appendChild(componentPresentationCard)
 
     return [
         $Home,
         [
-            stylesHeader
+            stylesHeader,
+            stylesPresentationCard
         ]
     ]
 }
