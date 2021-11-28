534caa0d54a404646ce13d9ff2a4ddcb9fc10e48
devFron <developermater26@gmail.com>
2021-11-28 17:00:44 -0600

    se agreo el componente de ContactForm y se destructuro como arreglo el la primera posicion se agrego como hijo de el componente de  y la segunda posicion se agrego como positions del arreglo de estilos de la pagina home

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,12 +1,14 @@
 import Header from '../../components/Header.js'
 import PresentationCard from '../../components/Presentation_card.js'
+import ContactForm from '../../components/Contact_form.js'
 const Home = ()=>{
     const $Home = document.createElement('section')
     const [componentHeader,stylesHeader] = Header()
     const [componentPresentationCard,stylesPresentationCard] = PresentationCard()
-
+    const [componentContactForm,stylesContactForm] = ContactForm()
     $Home.appendChild(componentHeader)
     $Home.appendChild(componentPresentationCard)
+    $Home.appendChild(componentContactForm)
     const stylesHome = {
         mobile:`
             body{
@@ -19,7 +21,8 @@ const Home = ()=>{
         [
             stylesHome,
             stylesHeader,
-            stylesPresentationCard
+            stylesPresentationCard,
+            stylesContactForm
         ]
     ]
 }
