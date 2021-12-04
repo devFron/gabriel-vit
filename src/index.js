cc9ef202fe8c2b415b93f2f59323515799605efc
devFron <developermater26@gmail.com>
2021-12-03 18:21:50 -0600

    se agrego un listener con el evento click al dom si el click lo ocaciona algun elemento que tenga la clase '.button__enabled' su comportamiento por efecto sera anulado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import App from './App.js'
 document.addEventListener('DOMContentLoaded',async(e)=>await App())
 window.addEventListener('hashchange',async(e)=>await App())
+document.addEventListener('click',(e)=>{
+    if(e.target.matches('.button__enabled')){e.preventDefault()}
+})
