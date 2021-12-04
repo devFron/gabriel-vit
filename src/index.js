c5ec848f3431e13233c78c46d8d0bded0f2fdada
devFron <developermater26@gmail.com>
2021-12-03 18:26:29 -0600

    se cambio el selector de validacion para los elementos discapacitados ahora sera el nombre del nodo htmlseguido de la palabra 'disabled'

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import App from './App.js'
 document.addEventListener('DOMContentLoaded',async(e)=>await App())
 window.addEventListener('hashchange',async(e)=>await App())
 document.addEventListener('click',(e)=>{
-    if(e.target.matches('.button__enabled')){e.preventDefault()}
+    if(e.target.matches('.button__disabled')){e.preventDefault()}
 })
 document.addEventListener('submit',(e)=>{
-    if(e.target.matches('.form__enabeld')){e.preventDefault()}
+    if(e.target.matches('.form__disabled')){e.preventDefault()}
 })
