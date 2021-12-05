0f7fddab21529127700ebda08426b9cd9c373147
devFron <developermater26@gmail.com>
2021-12-05 16:47:54 -0600

    se busca la etiqueta link que su href contiene main.css lugeo se inserta la etiqueta style dinamica mente despues de dicho elemento buscado

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import Router from './router.js'
 const App = async ()=>{
     const $ROOT = document.getElementById('root') 
     const $Loader = document.createElement('div')
+    const $style = document.createElement('style')
+    $style.id='dynamic-styles'
     $Loader.appendChild(Loader()[0])
+    const linkNode =document.querySelectorAll('link')
+    let linkCss = Array.from(linkNode).filter(node => node.href.includes('main.css'))
+    linkCss[0].insertAdjacentElement('afterend',$style)
     $ROOT.insertAdjacentElement('beforebegin',$Loader)
     await Router()
     $Loader.style.display='none'
