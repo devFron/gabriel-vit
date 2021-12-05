347c226cbcdff3ce5d2e324cf5e139dfd08263ae
devFron <developermater26@gmail.com>
2021-12-04 23:01:26 -0600

    se importaron los esilos en el index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import styles from '../public/styles.css'
 import App from './App.js'
 document.addEventListener('DOMContentLoaded',async(e)=>await App())
 window.addEventListener('hashchange',async(e)=>await App())
