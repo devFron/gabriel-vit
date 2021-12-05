379afdbd7f99ecc64f4f3aeaf6a9ad4daece6598
devFron <developermater26@gmail.com>
2021-12-05 16:49:16 -0600

    se retorna el elemento html del icono del cual se hace la peticion

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -4,5 +4,6 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 const Icon = ({prefix,iconName})=>{
     library.add(fas,fab)
     const IconRequest = icon({prefix,iconName})
+    return IconRequest.html[0]
 }
 export default Icon
